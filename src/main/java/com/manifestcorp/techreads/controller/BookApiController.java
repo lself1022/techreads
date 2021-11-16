a368eb7ba81470a882784240fb615c8c2d1dd08e
lself <lself@manifestcorp.com>
2021-11-16 12:58:31 -0500

    Added update and delete for Book API

--- src/main/java/com/manifestcorp/techreads/controller/BookApiController.java
+++ src/main/java/com/manifestcorp/techreads/controller/BookApiController.java
@@ -4,6 +4,7 @@ package com.manifestcorp.techreads.controller;
 import com.manifestcorp.techreads.model.Book;
 import com.manifestcorp.techreads.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -22,9 +23,8 @@ public class BookApiController {
     }
 
     @GetMapping("/{id}")
-    public Book findById(@PathVariable String id) {
-        Long bookId = Long.valueOf(id);
-        return bookRepository.findById(bookId).orElse(new Book());
+    public Book findById(@PathVariable Long id) {
+        return bookRepository.findById(id).orElse(new Book());
     }
 
     @PostMapping(path= "/", consumes = "application/json", produces = "application/json")
@@ -32,6 +32,17 @@ public class BookApiController {
         return bookRepository.saveAndFlush(newBook);
     }
 
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void update(@PathVariable Long id, @RequestBody Book book) {
+        book.setId(id);
+        bookRepository.saveAndFlush(book);
+    }
 
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+    }
 
 }
